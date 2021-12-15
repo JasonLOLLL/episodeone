c37f0fcfcf172c5b6da0919b334329a79eff84d4
BuildTools <unconfigured@null.spigotmc.org>
2021-12-14 19:29:22 -0500

    added spongebob boss

--- src/main/java/com/jasonjat/episodeone/entity/SpongebobBossEntity.java
+++ src/main/java/com/jasonjat/episodeone/entity/SpongebobBossEntity.java
@@ -1,29 +1,38 @@
 package com.jasonjat.episodeone.entity;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.IAnimationTickable;
 import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class SpongebobBossEntity extends HostileEntity implements IAnimatable {
+public class SpongebobBossEntity extends HostileEntity implements IAnimatable, IAnimationTickable {
 
     private final AnimationFactory factory = new AnimationFactory(this);
     private final ServerBossBar bossBar;
 
     @Override
     protected void initGoals() {
-        goalSelector.add(1, new MeleeAttackGoal(this, 1, true));
-        goalSelector.add(2, new WanderNearTargetGoal(this, 1, 15));
+        int maxSpeed = 2;
+        goalSelector.add(1, new MeleeAttackGoal(this, maxSpeed, true));
         goalSelector.add(5, new LookAroundGoal(this));
 
         targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
@@ -33,6 +42,7 @@ public class SpongebobBossEntity extends HostileEntity implements IAnimatable {
         super(entityType, world);
         this.ignoreCameraFrustum = true;
         this.bossBar = (ServerBossBar) (new ServerBossBar(this.getDisplayName(), BossBar.Color.YELLOW, BossBar.Style.PROGRESS)).setDarkenSky(true);
+        this.stepHeight = 4F;
     }
 
     @Override
@@ -59,11 +69,33 @@ public class SpongebobBossEntity extends HostileEntity implements IAnimatable {
     }
 
     private <T extends IAnimatable> PlayState predicate(AnimationEvent<T> event) {
-        return PlayState.STOP;
+        if (event.isMoving()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.spongebob_boss.sprint", true));
+        } else {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.spongebob_boss.idle", true));
+        }
+        return PlayState.CONTINUE;
     }
 
     @Override
     public AnimationFactory getFactory() {
         return factory;
+    }
+
+    @Override
+    public void onAttacking(Entity target) {
+        super.onAttacking(target);
+        ServerWorld world = (ServerWorld) target.getWorld();
+        world.spawnParticles(ParticleTypes.FLAME, target.getX(), target.getY(), target.getZ(), 25, 0,0,0, 1);
+    }
+
+    @Override
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+        return true;
+    }
+
+    @Override
+    public int tickTimer() {
+        return age;
     }
 }
--- src/main/java/com/jasonjat/episodeone/entity/model/SpongebobBossModel.java
+++ src/main/java/com/jasonjat/episodeone/entity/model/SpongebobBossModel.java
@@ -1,11 +1,16 @@
 package com.jasonjat.episodeone.entity.model;
 
+import com.jasonjat.episodeone.entity.PurpleGolemEntity;
 import com.jasonjat.episodeone.entity.SpongebobBossEntity;
 import com.jasonjat.episodeone.util.ModUtil;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.AnimatedTickingGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
-public class SpongebobBossModel extends AnimatedGeoModel<SpongebobBossEntity> {
+public class SpongebobBossModel extends AnimatedTickingGeoModel<SpongebobBossEntity> {
     @Override
     public Identifier getModelLocation(SpongebobBossEntity object) {
         return ModUtil.id("geo/spongebob_boss.geo.json");
@@ -19,5 +24,18 @@ public class SpongebobBossModel extends AnimatedGeoModel<SpongebobBossEntity> {
     @Override
     public Identifier getAnimationFileLocation(SpongebobBossEntity animatable) {
         return ModUtil.id("animations/spongebob_boss.animation.json");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setLivingAnimations(SpongebobBossEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        if (head != null) {
+            head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+            head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
+        }
     }
 }
